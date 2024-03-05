afd4ef2f881ee4acdf95c476c2c0cdb784f5f6c8
Guillaume Smith <gwil1248@fastmail.net>
2024-03-05 23:31:53 +1100

    Use array data with map for pizzas.

--- 03-pizza-menu/src/index.js
+++ 03-pizza-menu/src/index.js
@@ -69,7 +69,12 @@ function Menu() {
   return (
     <main className="menu">
       <h2>Our menu</h2>
-      <Pizza
+      <ul className="pizzas">
+        {pizzaData.map((pizza) => (
+          <Pizza pizzaObj={pizza} key={pizza.name} />
+        ))}
+      </ul>
+      {/* <Pizza
         name="Pizza Spinaci"
         ingredients="abcd"
         photoName="pizzas/spinaci.jpg"
@@ -80,21 +85,21 @@ function Menu() {
         ingredients="Tomatoes, mushrooms"
         photoName="pizzas/funghi.jpg"
         price={12}
-      />
+      /> */}
     </main>
   );
 }
 
 function Pizza(props) {
   return (
-    <div className="pizza">
-      <img src={props.photoName} alt={props.name} />
+    <li className="pizza">
+      <img src={props.pizzaObj.photoName} alt={props.pizzaObj.name} />
       <div>
-        <h3>{props.name}</h3>
-        <p>{props.ingredients}</p>
-        <span>{props.price}</span>
+        <h3>{props.pizzaObj.name}</h3>
+        <p>{props.pizzaObj.ingredients}</p>
+        <span>{props.pizzaObj.price}</span>
       </div>
-    </div>
+    </li>
   );
 }
 
