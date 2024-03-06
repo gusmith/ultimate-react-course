e1f7c44192e93516c41e8c4917c5255e65f6db2e
Guillaume Smith <gwil1248@fastmail.net>
2024-03-06 22:03:48 +1100

    Can return multiple time return statement in component instead of binary and ternary operator.

--- 03-pizza-menu/src/index.js
+++ 03-pizza-menu/src/index.js
@@ -87,6 +87,10 @@ function Menu() {
 }
 
 function Pizza(props) {
+  if (props.pizzaObj.soldOut) {
+    return null;
+  }
+
   return (
     <li className="pizza">
       <img src={props.pizzaObj.photoName} alt={props.pizzaObj.name} />
@@ -112,6 +116,15 @@ function Footer() {
   // }
 
   // return React.createElement("footer", null, "We're currently open!");
+
+  // if (!isOpen) {
+  //   return (
+  //     <p>
+  //       We're happy to welcome you between {openHour}:00 and {closeHour}:00.
+  //     </p>
+  //   );
+  // }
+
   return (
     <footer className="footer">
       {isOpen ? (
