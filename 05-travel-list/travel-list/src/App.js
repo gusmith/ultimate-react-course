f0ef52795e6ee0b8e474d401b68e5b012c5ddeca
Guillaume Smith <gwil1248@fastmail.net>
2024-03-19 23:17:56 +1100

    Derived state

--- 05-travel-list/travel-list/src/App.js
+++ 05-travel-list/travel-list/src/App.js
@@ -28,7 +28,7 @@ export default function App() {
         onDeleteItem={handleDeleteItem}
         onToogleItem={handleToggleItem}
       />
-      <Stats />
+      <Stats items={items} />
     </div>
   );
 }
@@ -115,10 +115,26 @@ function Item({ item, onDeleteItem, onToogleItem }) {
   );
 }
 
-function Stats() {
+function Stats({ items }) {
+  if (!items.length) {
+    return (
+      <p className="stats">
+        <em>Start adding some items to your packing list.</em>
+      </p>
+    );
+  }
+
+  const numItems = items.length;
+  const numPacked = items.filter((item) => item.packed).length;
+  const percentage = Math.round((numPacked / numItems) * 100);
   return (
     <footer className="stats">
-      <em>👜You have x items on your list, and you already packed x (x%).</em>
+      <em>
+        {percentage === 100
+          ? "You got everything! Ready to go."
+          : `👜You have ${numItems} items on your list, and you already packed
+        ${numPacked} (${percentage}%).`}
+      </em>
     </footer>
   );
 }
