7d94d120be6ebf7c43667b0b03b1ecb0c2f1b032
Guillaume Smith <gwil1248@fastmail.net>
2024-03-19 23:29:11 +1100

    Sort by button

--- 05-travel-list/travel-list/src/App.js
+++ 05-travel-list/travel-list/src/App.js
@@ -81,10 +81,25 @@ function Form({ onAddItem }) {
 }
 
 function PackingList({ items, onDeleteItem, onToogleItem }) {
+  const [sortBy, setSortBy] = useState("input");
+
+  let sortedItems;
+  if (sortBy === "input") {
+    sortedItems = items;
+  } else if (sortBy === "description") {
+    sortedItems = items
+      .slice()
+      .sort((a, b) => a.description.localeCompare(b.description));
+  } else if (sortBy === "packed") {
+    sortedItems = items
+      .slice()
+      .sort((a, b) => Number(a.packed) - Number(b.packed));
+  }
+
   return (
     <div className="list">
       <ul>
-        {items.map((item) => (
+        {sortedItems.map((item) => (
           <Item
             item={item}
             key={item.id}
@@ -93,6 +108,13 @@ function PackingList({ items, onDeleteItem, onToogleItem }) {
           />
         ))}
       </ul>
+      <div className="actions">
+        <select value={sortBy} onChange={(e) => setSortBy(e.target.value)}>
+          <option value="input">Sort by input order</option>
+          <option value="description">Sort by description</option>
+          <option value="packed">Sort by packed status</option>
+        </select>
+      </div>
     </div>
   );
 }
