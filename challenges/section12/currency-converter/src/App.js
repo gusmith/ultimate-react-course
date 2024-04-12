8a531d4c736da93862160ead123a8bb096fd13d7
Guillaume Smith <guillaume.smith@ambiata.com>
2024-04-12 13:56:49 +1000

    Use effect instead of using the handlers.

--- challenges/section12/currency-converter/src/App.js
+++ challenges/section12/currency-converter/src/App.js
@@ -1,36 +1,57 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 export default function App() {
   const [from, setFrom] = useState("USD");
   const [to, setTo] = useState("EUR");
   const [input, setInput] = useState("");
   const [output, setOutput] = useState(null);
-
-  async function fecthing(f, t, i) {
-    const res = await fetch(
-      `https://api.frankfurter.app/latest?amount=${i}&from=${f}&to=${t}`
-    );
-    const data = await res.json();
-    setOutput(data?.rates ? data.rates[t] : null);
-  }
+  const [isLoading, setIsLoading] = useState(false);
 
   function handleFromChange(e) {
     const f = e.target.value;
     setFrom(f);
-    fecthing(f, to, input);
   }
 
   function handleToChange(e) {
     const t = e.target.value;
     setTo(t);
-    fecthing(from, t, input);
   }
 
   function handleInputChange(e) {
     const i = Number(e.target.value);
     setInput(i);
-    fecthing(from, to, i);
   }
+
+  useEffect(
+    function () {
+      const abort = new AbortController();
+      async function fecthing() {
+        try {
+          setIsLoading(true);
+          const res = await fetch(
+            `https://api.frankfurter.app/latest?amount=${input}&from=${from}&to=${to}`,
+            { signal: abort.signal }
+          );
+          const data = await res.json();
+          setOutput(data?.rates ? data.rates[to] : null);
+        } catch (err) {
+          console.log(err.message);
+          setOutput(null);
+        } finally {
+          setIsLoading(false);
+        }
+      }
+      if (!input || from === to) {
+        setOutput(null);
+        return;
+      }
+      fecthing();
+      return () => {
+        abort.abort();
+      };
+    },
+    [input, from, to]
+  );
 
   return (
     <div>
@@ -47,7 +68,7 @@ export default function App() {
         <option value="CAD">CAD</option>
         <option value="INR">INR</option>
       </select>
-      <p>{output}</p>
+      <p>{isLoading ? "Loading" : output}</p>
     </div>
   );
 }
