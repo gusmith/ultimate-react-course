f695323534a77d9a81021f8c10eb9e733bc74087
Guillaume Smith <guillaume.smith@ambiata.com>
2024-04-18 14:16:53 +1000

    Components folder and question component

---
+++ 10-react-quiz/react-quiz/src/components/Options.js
@@ -0,0 +1,11 @@
+export default function Options({ options }) {
+  return (
+    <div className="options">
+      {options.map((option) => (
+        <button key={option} className="btn btn-option">
+          {option}
+        </button>
+      ))}
+    </div>
+  );
+}
