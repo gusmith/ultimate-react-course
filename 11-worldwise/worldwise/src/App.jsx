0d4ed0155a4530d059a3ce557d0309603e98689c
Guillaume Smith <guillaume.smith@ambiata.com>
2024-04-19 11:28:09 +1000

    Create few pages and routes

---
+++ 11-worldwise/worldwise/src/App.jsx
@@ -0,0 +1,18 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Product from "./pages/Product";
+import Pricing from "./pages/Pricing";
+import Homepgae from "./pages/Homepage";
+import PageNotFound from "./pages/PageNotFound";
+
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Homepgae />} />
+        <Route path="product" element={<Product />} />
+        <Route path="pricing" element={<Pricing />} />
+        <Route path="*" element={<PageNotFound />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
---
+++ 11-worldwise/worldwise/src/pages/Homepage.jsx
@@ -0,0 +1,3 @@
+export default function Homepgae() {
+  return <div>Homepage</div>;
+}
