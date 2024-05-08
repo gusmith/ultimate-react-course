d53805e0aef07290befa6c7e2ad0f8ebb00a72e9
Guillaume Smith <guillaume.smith@ambiata.com>
2024-05-08 15:12:13 +1000

    Ability to get position in form and move the map center

--- 11-worldwise/worldwise/src/components/Map.jsx
+++ 11-worldwise/worldwise/src/components/Map.jsx
@@ -1,7 +1,15 @@
 import { useNavigate, useSearchParams } from "react-router-dom";
 import styles from "./Map.module.css";
-import { MapContainer, Marker, TileLayer, Popup } from "react-leaflet";
-import { useState } from "react";
+import {
+  MapContainer,
+  Marker,
+  TileLayer,
+  Popup,
+  useMap,
+  useMapEvent,
+  useMapEvents,
+} from "react-leaflet";
+import { useState, useEffect } from "react";
 import { useCities } from "../contexts/CitiesContext";
 
 export default function Map() {
@@ -11,14 +19,21 @@ export default function Map() {
   const [mapPosition, setMapPosition] = useState([40, 0]);
 
   const [searchParams, setSearchParams] = useSearchParams();
-  const lat = searchParams.get("lat");
-  const lng = searchParams.get("lng");
+  const mapLat = searchParams.get("lat");
+  const mapLgn = searchParams.get("lng");
+
+  useEffect(
+    function () {
+      if (mapLat && mapLgn) setMapPosition([mapLat, mapLgn]);
+    },
+    [mapLat, mapLgn]
+  );
 
   return (
     <div className={styles.mapContainer}>
       <MapContainer
-        center={[lat ? lat : 40, lng ? lng : 0]}
-        zoom={13}
+        center={mapPosition}
+        zoom={6}
         scrollWheelZoom={true}
         className={styles.map}
       >
@@ -37,7 +52,24 @@ export default function Map() {
             </Popup>
           </Marker>
         ))}
+        <ChangeCenter position={mapPosition} />
+        <DetectClick />
       </MapContainer>
     </div>
   );
+}
+
+function ChangeCenter({ position }) {
+  const map = useMap();
+  map.setView(position);
+  return null;
+}
+
+function DetectClick() {
+  const navigate = useNavigate();
+  useMapEvents({
+    click: (e) => {
+      navigate(`form?lat=${e.latlng.lat}&lng=${e.latlng.lng}`);
+    },
+  });
 }
