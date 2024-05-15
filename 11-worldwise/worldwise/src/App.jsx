5cde204c06bf6b8dc9500e036abbafd578191e51
Guillaume Smith <guillaume.smith@ambiata.com>
2024-05-15 13:40:08 +1000

    Protect pages when unauthenticated.

--- 11-worldwise/worldwise/src/App.jsx
+++ 11-worldwise/worldwise/src/App.jsx
@@ -11,6 +11,7 @@ import CountryList from "./components/CountryList";
 import Form from "./components/Form";
 import { CitiesProvider } from "./contexts/CitiesContext";
 import { AuthProvider } from "./contexts/FakeAuthContext";
+import ProtectedRoute from "./pages/ProtectedRoute";
 
 export default function App() {
   return (
@@ -22,7 +23,14 @@ export default function App() {
             <Route path="product" element={<Product />} />
             <Route path="pricing" element={<Pricing />} />
             <Route path="login" element={<Login />} />
-            <Route path="app" element={<AppLayout />}>
+            <Route
+              path="app"
+              element={
+                <ProtectedRoute>
+                  <AppLayout />
+                </ProtectedRoute>
+              }
+            >
               {/* By stating index, it is the default is no subpath is provided, replace ensures we can press back jumping above this redirection */}
               <Route index element={<Navigate replace to="cities" />} />
               <Route path="cities" element={<CityList />} />
