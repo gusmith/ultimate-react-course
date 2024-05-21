5b5aa5e43a8019b8db696cc9097aeee62f4f98e5
Guillaume Smith <guillaume.smith@ambiata.com>
2024-05-21 14:59:01 +1000

    Split redux file into recent code structure.

---
+++ 15-redux-intro/redux-intro/src/features/accounts/accountSlide.js
@@ -0,0 +1,56 @@
+// Slice, describing it is a slice of the whole redux state
+
+const initialStateAccount = {
+  balance: 0,
+  loan: 0,
+  loanPurpose: "",
+};
+
+export default function accountReducer(state = initialStateAccount, action) {
+  switch (action.type) {
+    case "account/deposit":
+      return { ...state, balance: state.balance + action.payload };
+    case "account/withdraw":
+      return { ...state, balance: state.balance - action.payload };
+    case "account/requestLoan":
+      if (state.loan > 0) return state;
+      return {
+        ...state,
+        loan: action.payload.amount,
+        loanPurpose: action.payload.loanPurpose,
+        balance: state.balance + action.payload.amount,
+      };
+    case "account/payLoan":
+      return {
+        ...state,
+        loan: 0,
+        loanPurpose: "",
+        balance: state.balance - state.loan,
+      };
+    default:
+      // Redux recommend returning the initial state.
+      return state;
+  }
+}
+
+// Convention with Redux: action creators
+export function deposit(amount) {
+  return { type: "account/deposit", payload: amount };
+}
+
+export function withdraw(amount) {
+  return { type: "account/withdraw", payload: amount };
+}
+
+export function requestLoan(amount, loanPurpose) {
+  return {
+    type: "account/requestLoan",
+    payload: { amount: amount, loanPurpose: loanPurpose },
+  };
+}
+
+export function payLoan() {
+  return {
+    type: "account/payLoan",
+  };
+}
