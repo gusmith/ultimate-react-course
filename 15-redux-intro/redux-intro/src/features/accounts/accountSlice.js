ba18edd99cd505a8b9ba1ef899d27c2e5cfbed3c
Guillaume Smith <guillaume.smith@ambiata.com>
2024-05-22 11:46:13 +1000

    Slice for the account re-writen for redux toolkit

--- 15-redux-intro/redux-intro/src/features/accounts/accountSlice.js
+++ 15-redux-intro/redux-intro/src/features/accounts/accountSlice.js
@@ -1,74 +1,50 @@
+import { createSlice } from "@reduxjs/toolkit";
+
 // Slice, describing it is a slice of the whole redux state
 
-const initialStateAccount = {
+const initialState = {
   balance: 0,
   loan: 0,
   loanPurpose: "",
   isLoading: false,
 };
 
-export default function accountReducer(state = initialStateAccount, action) {
-  switch (action.type) {
-    case "account/deposit":
-      return {
-        ...state,
-        balance: state.balance + action.payload,
-        isLoading: false,
-      };
-    case "account/withdraw":
-      return { ...state, balance: state.balance - action.payload };
-    case "account/requestLoan":
-      if (state.loan > 0) return state;
-      return {
-        ...state,
-        loan: action.payload.amount,
-        loanPurpose: action.payload.loanPurpose,
-        balance: state.balance + action.payload.amount,
-      };
-    case "account/payLoan":
-      return {
-        ...state,
-        loan: 0,
-        loanPurpose: "",
-        balance: state.balance - state.loan,
-      };
-    case "account/convertingCurrency":
-      return { ...state, isLoading: true };
-    default:
-      // Redux recommend returning the initial state.
-      return state;
-  }
-}
+const accountSlice = createSlice({
+  name: "account",
+  initialState: initialState,
+  reducers: {
+    deposit(state, action) {
+      // In Redux toolkit, we can mutate the state, but under the hood, redux toolkit is making it immutable and we do not return anything
+      state.balance += action.payload;
+    },
+    withdraw(state, action) {
+      state.balance -= action.payload;
+    },
+    requestLoan: {
+      // Prepare is ran before the reducer, to prepare the action object if we want multiple arguments in the action creator.
+      prepare(amount, purpose) {
+        return {
+          payload: {
+            amount,
+            loanPurpose: purpose,
+          },
+        };
+      },
+      reducer(state, action) {
+        if (state.loan > 0) return;
+        state.loan = action.payload.amount;
+        state.loanPurpose = action.payload.loanPurpose;
+        state.balance += action.payload.amount;
+      },
+    },
+    payLoan(state, action) {
+      state.balance -= state.loan;
+      state.loan = 0;
+      state.loanPurpose = "";
+    },
+  },
+});
 
-// Convention with Redux: action creators
-export function deposit(amount, currency) {
-  if (currency === "USD") return { type: "account/deposit", payload: amount };
-  // Because it is a function, redux will know it needs to go in the middleware, which is in our case thunk
-  return async function (dispatch, getState) {
-    dispatch({ type: "account/convertingCurrency" });
-    // API call
-    const res = await fetch(
-      `https://api.frankfurter.app/latest?amount=${amount}&from=${currency}&to=USD`
-    );
-    const data = await res.json();
-    const converted = data.rates.USD;
-    dispatch({ type: "account/deposit", payload: converted });
-  };
-}
+export const { deposit, withdraw, requestLoan, payLoan } = accountSlice.actions;
 
-export function withdraw(amount) {
-  return { type: "account/withdraw", payload: amount };
-}
-
-export function requestLoan(amount, loanPurpose) {
-  return {
-    type: "account/requestLoan",
-    payload: { amount: amount, loanPurpose: loanPurpose },
-  };
-}
-
-export function payLoan() {
-  return {
-    type: "account/payLoan",
-  };
-}
+export default accountSlice.reducer;
---
+++ 15-redux-intro/redux-intro/src/features/accounts/accountSlieClassicRedux.js
@@ -0,0 +1,74 @@
+// Slice, describing it is a slice of the whole redux state
+
+const initialStateAccount = {
+  balance: 0,
+  loan: 0,
+  loanPurpose: "",
+  isLoading: false,
+};
+
+export default function accountReducer(state = initialStateAccount, action) {
+  switch (action.type) {
+    case "account/deposit":
+      return {
+        ...state,
+        balance: state.balance + action.payload,
+        isLoading: false,
+      };
+    case "account/withdraw":
+      return { ...state, balance: state.balance - action.payload };
+    case "account/requestLoan":
+      if (state.loan > 0) return state;
+      return {
+        ...state,
+        loan: action.payload.amount,
+        loanPurpose: action.payload.loanPurpose,
+        balance: state.balance + action.payload.amount,
+      };
+    case "account/payLoan":
+      return {
+        ...state,
+        loan: 0,
+        loanPurpose: "",
+        balance: state.balance - state.loan,
+      };
+    case "account/convertingCurrency":
+      return { ...state, isLoading: true };
+    default:
+      // Redux recommend returning the initial state.
+      return state;
+  }
+}
+
+// Convention with Redux: action creators
+export function deposit(amount, currency) {
+  if (currency === "USD") return { type: "account/deposit", payload: amount };
+  // Because it is a function, redux will know it needs to go in the middleware, which is in our case thunk
+  return async function (dispatch, getState) {
+    dispatch({ type: "account/convertingCurrency" });
+    // API call
+    const res = await fetch(
+      `https://api.frankfurter.app/latest?amount=${amount}&from=${currency}&to=USD`
+    );
+    const data = await res.json();
+    const converted = data.rates.USD;
+    dispatch({ type: "account/deposit", payload: converted });
+  };
+}
+
+export function withdraw(amount) {
+  return { type: "account/withdraw", payload: amount };
+}
+
+export function requestLoan(amount, loanPurpose) {
+  return {
+    type: "account/requestLoan",
+    payload: { amount: amount, loanPurpose: loanPurpose },
+  };
+}
+
+export function payLoan() {
+  return {
+    type: "account/payLoan",
+  };
+}
