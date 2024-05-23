c6b8bd7e263df698f6e109532c7147873f7ccc6b
Guillaume Smith <guillaume.smith@ambiata.com>
2024-05-23 14:49:45 +1000

    Navgiation state to check if the route is loading

--- 16-fast-react-pizza/fast-react-pizza/src/ui/AppLayout.jsx
+++ 16-fast-react-pizza/fast-react-pizza/src/ui/AppLayout.jsx
@@ -1,10 +1,14 @@
 import Header from "./Header";
 import CartOverview from "../features/cart/CartOverview";
-import { Outlet } from "react-router-dom/dist";
+import { Outlet, useNavigation } from "react-router-dom/dist";
+import Loader from "./Loader";
 
 export default function AppLayout() {
+  const navigation = useNavigation();
+  const isLoading = navigation.state === "loading";
   return (
-    <div>
+    <div className="layout">
+      {isLoading && <Loader />}
       <Header />
       <main>
         <Outlet />
