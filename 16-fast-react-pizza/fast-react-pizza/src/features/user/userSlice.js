ee1b59e94aa5bac402d964580ff43959bf4d6bcb
Guillaume Smith <guillaume.smith@ambiata.com>
2024-05-23 11:47:24 +1000

    Fix some import issues.

--- 16-fast-react-pizza/fast-react-pizza/src/features/user/userSlice.js
+++ 16-fast-react-pizza/fast-react-pizza/src/features/user/userSlice.js
@@ -1,3 +1,5 @@
+import { getAddress } from "../../utils/helpers";
+
 function getPosition() {
   return new Promise(function (resolve, reject) {
     navigator.geolocation.getCurrentPosition(resolve, reject);
