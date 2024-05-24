c4c5ebeaccf11451ad299d0b9037fad8a452c59e
Guillaume Smith <guillaume.smith@ambiata.com>
2024-05-24 16:30:51 +1000

    Cart slice created

---
+++ 16-fast-react-pizza/fast-react-pizza/src/features/cart/cartSlice.js
@@ -0,0 +1,65 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  //   cart: [],
+  cart: [
+    {
+      pizzaId: 12,
+      name: "Mediterranean",
+      quantity: 2,
+      unitPrice: 16,
+      totalPrice: 32,
+    },
+  ],
+};
+
+const cartSlice = createSlice({
+  name: "cart",
+  initialState: initialState,
+  reducers: {
+    addItem(state, action) {
+      // payload = newItem
+      state.cart.push(action.payload);
+    },
+    deleteItem(state, action) {
+      // payload = pizzaId
+      state.cart = state.cart.filter((item) => item.pizzaId !== action.payload);
+    },
+    increaseItemQuantity(state, action) {
+      // payload = pizzaId
+      const item = state.cart.find((item) => item.pizzaId === action.payload);
+      if (item) {
+        item.quantity++;
+        item.totalPrice += item.unitPrice;
+      }
+    },
+    decreaseItemQuantity(state, action) {
+      // payload = pizzaId
+      const item = state.cart.find((item) => item.pizzaId === action.payload);
+      if (!item) return;
+      if (item.quantity === 1) {
+        state.cart = state.cart.filter(
+          (item) => item.pizzaId !== action.payload,
+        );
+        return;
+      }
+      if (item.quantity > 0) {
+        item.quantity--;
+        item.totalPrice -= item.unitPrice;
+      }
+    },
+    clearCart(state) {
+      state.cart = [];
+    },
+  },
+});
+
+export const {
+  addItem,
+  deleteItem,
+  increaseItemQuantity,
+  decreaseItemQuantity,
+  clearCart,
+} = cartSlice.actions;
+
+export default cartSlice.reducer;
