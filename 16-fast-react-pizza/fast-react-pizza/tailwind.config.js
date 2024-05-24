6e8030db1e169c4b2fedff7798da4159d2fb1b29
Guillaume Smith <guillaume.smith@ambiata.com>
2024-05-24 14:15:50 +1000

    Configuring some tailwind configuration

--- 16-fast-react-pizza/fast-react-pizza/tailwind.config.js
+++ 16-fast-react-pizza/fast-react-pizza/tailwind.config.js
@@ -1,8 +1,15 @@
 /** @type {import('tailwindcss').Config} */
+// eslint-disable-next-line
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    fontFamily: { sans: "Roboto Mono, monospace" }, // sans is the default font
+    extend: {
+      fontSize: { huge: ["80rem", { lineHeight: "1" }] },
+      heigth: { screen: "100dvh" },
+    },
   },
   plugins: [],
 };
+
+//The base config is in https://github.com/tailwindlabs/tailwindcss/blob/master/stubs/config.full.js which can be overwriten and extended in this file
