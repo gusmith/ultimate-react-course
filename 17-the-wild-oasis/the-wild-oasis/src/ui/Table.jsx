67f3db0c9b7a7c336f5cfae15c83d49bab1500b8
Guillaume Smith <guillaume.smith@ambiata.com>
2024-06-12 11:26:41 +1000

    Fish the reusable table

--- 17-the-wild-oasis/the-wild-oasis/src/ui/Table.jsx
+++ 17-the-wild-oasis/the-wild-oasis/src/ui/Table.jsx
@@ -78,6 +78,7 @@ function Header({ children }) {
     </StyledHeader>
   );
 }
+
 function Row({ children }) {
   const { columns } = useContext(TableContext);
   return (
@@ -86,7 +87,11 @@ function Row({ children }) {
     </StyledRow>
   );
 }
-function Body({ children }) {}
+
+function Body({ data, render }) {
+  if (data.length === 0) return <Empty>No data to show at the moment</Empty>;
+  return <StyledBody>{data.map(render)}</StyledBody>;
+}
 
 Table.Header = Header;
 Table.Row = Row;
