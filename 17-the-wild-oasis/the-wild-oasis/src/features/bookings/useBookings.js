a20f0ed3bdebcb6b1b4b04e1539f96d87815d565
Guillaume Smith <guillaume.smith@ambiata.com>
2024-06-14 10:10:13 +1000

    Filter the bookings using the filtering capability of the backend

--- 17-the-wild-oasis/the-wild-oasis/src/features/bookings/useBookings.js
+++ 17-the-wild-oasis/the-wild-oasis/src/features/bookings/useBookings.js
@@ -1,14 +1,24 @@
 import { useQuery } from "@tanstack/react-query";
 import { getBookings } from "../../services/apiBookings";
+import { useSearchParams } from "react-router-dom";
 
 export function useBookings() {
+  const [searchParams] = useSearchParams();
+
+  // FILTER
+  const filterValue = searchParams.get("status");
+  const filter =
+    !filterValue || filterValue === "all"
+      ? null
+      : { field: "status", value: filterValue, method: "eq" };
+
   const {
     isLoading,
     data: bookings,
     error,
   } = useQuery({
-    queryKey: ["bookings"], // Uniquely identify the query
-    queryFn: getBookings, // function for querying, must return a promise
+    queryKey: ["bookings", filter], // Uniquely identify the query
+    queryFn: () => getBookings({ filter }), // function for querying, must return a promise
   });
   return { isLoading, bookings, error };
 }
