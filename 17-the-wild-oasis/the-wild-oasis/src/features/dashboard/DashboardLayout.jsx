62a93f8cd237a6a12d6059b119590aa72fa57d20
Guillaume Smith <guillaume.smith@ambiata.com>
2024-07-03 14:57:52 +1000

    Prepare the query hooks for the dashboard

--- 17-the-wild-oasis/the-wild-oasis/src/features/dashboard/DashboardLayout.jsx
+++ 17-the-wild-oasis/the-wild-oasis/src/features/dashboard/DashboardLayout.jsx
@@ -1,5 +1,9 @@
 import styled from "styled-components";
 
+import { useRecentBookings } from "./useRecentBookings";
+import Spinner from "../../ui/Spinner";
+import { useRecentStays } from "./useRecentStays";
+
 const StyledDashboardLayout = styled.div`
   display: grid;
   grid-template-columns: 1fr 1fr 1fr 1fr;
@@ -8,11 +12,16 @@ const StyledDashboardLayout = styled.div`
 `;
 
 export default function DashboardLayout() {
+  const { bookings, isLoading: isLoading1 } = useRecentBookings();
+  const { stays, confirmedStays, isLoading: isLoading2 } = useRecentStays();
+
+  if (isLoading1 || isLoading2) return <Spinner />;
+
   return (
     <StyledDashboardLayout>
       <div>Statistics</div>
       <div>Today&apos;s activity</div>
-      <div>Chart stau durations</div>
+      <div>Chart stay durations</div>
       <div>Chart sales</div>
     </StyledDashboardLayout>
   );
