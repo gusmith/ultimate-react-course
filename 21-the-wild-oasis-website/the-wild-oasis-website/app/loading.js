13f6e4840458654c446233bdb76f777b479767da
Guillaume Smith <guillaume.smith@ambiata.com>
2024-07-19 10:18:57 +1000

    New loading page for the cabins routes

--- 21-the-wild-oasis-website/the-wild-oasis-website/app/loading.js
+++ 21-the-wild-oasis-website/the-wild-oasis-website/app/loading.js
@@ -1,5 +1,7 @@
 // Global loading page, used all the time when server components are getting loaded
 
+import Spinner from "@/app/_components/Spinner";
+
 export default function Loading() {
-  return <p>LOADING DATA...</p>;
+  return <Spinner />;
 }
