9ced96d88c9bbdc8df78d3e8114bbf8f2e009d4c
Guillaume Smith <guillaume.smith@ambiata.com>
2024-07-29 09:46:38 +1000

    Server action to update the guest

--- 21-the-wild-oasis-website/the-wild-oasis-website/app/_components/UpdateProfileForm.js
+++ 21-the-wild-oasis-website/the-wild-oasis-website/app/_components/UpdateProfileForm.js
@@ -1,19 +1,24 @@
 "use client";
 
 import { useState } from "react";
+import { updateGuest } from "@/app/_lib/actions";
 
-export default function UpdateProfileForm({ children }) {
+export default function UpdateProfileForm({ children, guest }) {
   const [count, setCount] = useState();
 
-  // CHANGE
-  const countryFlag = "pt.jpg";
+  const { fullName, email, nationality, nationalID, countryFlag } = guest;
 
   return (
-    <form className="bg-primary-900 py-8 px-12 text-lg flex gap-6 flex-col">
+    <form
+      className="bg-primary-900 py-8 px-12 text-lg flex gap-6 flex-col"
+      action={updateGuest}
+    >
       <div className="space-y-2">
         <label>Full name</label>
         <input
           disabled
+          defaultValue={fullName}
+          name="fullName"
           className="px-5 py-3 bg-primary-200 text-primary-800 w-full shadow-sm rounded-sm disabled:cursor-not-allowed disabled:bg-gray-600 disabled:text-gray-400"
         />
       </div>
@@ -22,6 +27,8 @@ export default function UpdateProfileForm({ children }) {
         <label>Email address</label>
         <input
           disabled
+          defaultValue={email}
+          name="email"
           className="px-5 py-3 bg-primary-200 text-primary-800 w-full shadow-sm rounded-sm disabled:cursor-not-allowed disabled:bg-gray-600 disabled:text-gray-400"
         />
       </div>
@@ -41,6 +48,7 @@ export default function UpdateProfileForm({ children }) {
       <div className="space-y-2">
         <label htmlFor="nationalID">National ID number</label>
         <input
+          defaultValue={nationalID}
           name="nationalID"
           className="px-5 py-3 bg-primary-200 text-primary-800 w-full shadow-sm rounded-sm"
         />
--- 21-the-wild-oasis-website/the-wild-oasis-website/app/_lib/actions.js
+++ 21-the-wild-oasis-website/the-wild-oasis-website/app/_lib/actions.js
@@ -1,8 +1,10 @@
 "use server";
-
-import { signIn, signOut } from "@/app/_lib/auth";
-
 // server actions
+
+import { auth, signIn, signOut } from "@/app/_lib/auth";
+import { supabase } from "@/app/_lib/supabase";
+
+const VALIDNATIONALIDREGEX = /^[a-zA-Z0-9]{6,12}$/;
 
 export async function signInAction() {
   await signIn("github", { redirectTo: "/account" });
@@ -10,4 +12,27 @@ export async function signInAction() {
 
 export async function signOutAction() {
   await signOut({ redirectTo: "/" });
+}
+
+export async function updateGuest(formData) {
+  const session = await auth();
+  if (!session) throw new Error("You must be logged in");
+
+  const nationalID = formData.get("nationalID");
+  const [nationality, countryFlag] = formData.get("nationality").split("%");
+
+  if (!VALIDNATIONALIDREGEX.test(nationalID)) {
+    throw new Error("Please provide a valid nationalID");
+  }
+
+  const updatedData = { nationality, countryFlag, nationalID };
+
+  const { error } = await supabase
+    .from("guests")
+    .update(updatedData)
+    .eq("id", session.user.guestId);
+
+  if (error) {
+    throw new Error("Guest could not be updated");
+  }
 }
