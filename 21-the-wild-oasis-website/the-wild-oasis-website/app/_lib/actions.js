202cf9c8c201a9f51adb19191aab6a83b8abe969
Guillaume Smith <guillaume.smith@ambiata.com>
2024-07-29 10:44:12 +1000

    Ability to delete a booking

--- 21-the-wild-oasis-website/the-wild-oasis-website/app/_lib/actions.js
+++ 21-the-wild-oasis-website/the-wild-oasis-website/app/_lib/actions.js
@@ -5,6 +5,7 @@ import { revalidatePath } from "next/cache";
 
 import { auth, signIn, signOut } from "@/app/_lib/auth";
 import { supabase } from "@/app/_lib/supabase";
+import { getBookings } from "./data-service";
 
 const VALIDNATIONALIDREGEX = /^[a-zA-Z0-9]{6,12}$/;
 
@@ -39,4 +40,26 @@ export async function updateGuest(formData) {
   }
 
   revalidatePath("/account/profile");
+}
+
+export async function deleteReservation(bookingId) {
+  const session = await auth();
+  if (!session) throw new Error("You must be logged in");
+
+  const guestBookings = await getBookings(session.user.guestId);
+  const guestBookingIds = guestBookings.map((booking) => booking.id);
+
+  if (!guestBookingIds.includes(bookingId)) {
+    throw new Error("You are not allowed to delete this booking");
+  }
+
+  const { error } = await supabase
+    .from("bookings")
+    .delete()
+    .eq("id", bookingId);
+
+  if (error) {
+    throw new Error("Booking could not be deleted");
+  }
+  revalidatePath("/account/reservations");
 }
